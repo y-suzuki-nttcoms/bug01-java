af9827e6cf3be0e7ace429d6900908b779c123fa
y.suzuki <y.suzuki@SNW18067.allnavi.nttnavi.co.jp>
2020-07-07 15:32:55 +0900

    add bug code

---
+++ BugA.java
@@ -0,0 +1,22 @@
+
+public class BugA {
+
+	public static void main(String[] args) {
+	    int n = 0;
+	
+	    switch (n) {
+	        case 0:
+	            System.out.println("ケース0。");
+	            // break文を記述していない。
+	        case 1:
+	            System.out.println("ケース1。");
+	            break;
+	        case 2:
+	            System.out.println("ケース2。");
+	            break;
+	        default:
+	            System.out.println("デフォルト。");
+	    }
+	}
+}
+
